9d04d565c164ea848fe4549e6fe27e645a8d712c
Glitch (hello-webpage) <none>
2020-05-07 20:55:58 +0000

    📒🗻 Checkpoint ./script.js:15951174/5067

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
  *    loadImage,
  *    random,
  *    square,
+ *    tint,
  */
 
 const ASPECT_RATIO = 16 / 9;
@@ -17,8 +18,9 @@ const SCREEN_WIDTH = Math.round(SCREEN_HEIGHT * ASPECT_RATIO);
 const LOGO_SIZE = 100;
 let DVD_IMAGE;
 
-let x = 0, y = 0;
-let xDelta = 1, yDelta = 1;
+const LOGO_COUNT = 3;
+
+let logos;
 
 function setup() {
   // Code here runs only once
@@ -27,34 +29,47 @@ function setup() {
 
   DVD_IMAGE = loadImage(
       "https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
+  
+  logos = [];
+  
+  for (let i = 0; i < LOGO_COUNT; i++) {
+    logos.push({
+      x: random(0, SCREEN_WIDTH - LOGO_SIZE),
+      y: random(0, SCREEN_HEIGHT - LOGO_SIZE),
+      xDelta: random([-1, 1]),
+      yDelta: random([-1, 1]),
+      color: randomColor(),
+    })
+  }
 }
 
 function draw() {
   // Code here runs continuously
 
-  // move Logo
-  x = x + xDelta;
-  if (x === 0 && xDelta === -1) {
-    xDelta = 1;
-    fill(randomColor());
-  } else if (x + LOGO_SIZE === SCREEN_WIDTH && xDelta === 1) {
-    xDelta = -1;
-    fill(randomColor());
-  }
+  // move Logos
+  for (const logo of logos) {
+    logo.x = logo.x + logo.xDelta;
+    if (logo.x === 0 && logo.xDelta === -1) {
+      logo.xDelta = 1;
+      logo.color = randomColor();
+    } else if (logo.x + LOGO_SIZE === SCREEN_WIDTH && logo.xDelta === 1) {
+      logo.xDelta = -1;
+      logo.color = randomColor();
+    }
 
-  y = y + yDelta;
-  if (y === 0 && yDelta === -1) {
-    yDelta = 1;
-    fill(randomColor());
-  } else if (y + LOGO_SIZE === SCREEN_HEIGHT && yDelta === 1) {
-    yDelta = -1;
-    fill(randomColor());
-  }
+    logo.y = logo.y + logo.yDelta;
+    if (logo.y === 0 && logo.yDelta === -1) {
+      logo.yDelta = 1;
+      logo.color = randomColor();
+    } else if (logo.y + LOGO_SIZE === SCREEN_HEIGHT && logo.yDelta === 1) {
+      logo.yDelta = -1;
+      logo.color = randomColor();
+    }
 
-  // draw Logo
-  fill('blue');
-  image(DVD_IMAGE, x, y, LOGO_SIZE, LOGO_SIZE);
-  // square(x, y, LOGO_SIZE, 10, 10, 10, 10);
+    // draw Logo
+    tint(logo.color);
+    image(DVD_IMAGE, logo.x, logo.y, LOGO_SIZE, LOGO_SIZE);
+  }
 }
 
 function randomColor() {
