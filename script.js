6b6acec655a557b9b6d5ef8ae995368e1717e946
Glitch (hello-webpage) <none>
2020-05-07 20:33:00 +0000

    🍬👯 Checkpoint ./script.js:15951174/3473

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 // Name any p5.js functions we use in the global so Glitch can recognize them.
 /* global square, createCanvas, random, background, fill, color, rect, ellipse, stroke, noStroke, noFill, strokeWeight */
 
-function setup(){
-  // Code here runs only once
-  createCanvas(300, 300);
-}
+const ASPECT_RATIO = 16 / 9;
+const SCREEN_HEIGHT = 300;
+const SCREEN_WIDTH = Math.round(SCREEN_HEIGHT * ASPECT_RATIO);
 
-let x = 0, y = 0, size = 50;
+const LOGO_SIZE = 50;
+
+let x = 0, y = 0;
+let xDelta = 1, yDelta = 1;
+
+function setup() {
+  // Code here runs only once
+  createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+  background(0);
+}
 
 function draw() {
   // Code here runs continuously
-  background(0);
-  
+
   // move Logo
-  
+  x = x + xDelta;
+  if (x === 0 && xDelta === -1) {
+    xDelta = 1;
+  } else if (x + LOGO_SIZE === SCREEN_WIDTH && xDelta === 1) {
+    xDelta = -1;
+  }
+
+  y = y + yDelta;
+  if (y === 0 && yDelta === -1) {
+    yDelta = 1;
+  } else if (y + LOGO_SIZE === SCREEN_HEIGHT && yDelta === 1) {
+    yDelta = -1;
+  }
+
   // draw Logo
   fill('blue');
-  square(0, 0, 50, 10);
+  square(x, y, LOGO_SIZE, 10, 10, 10, 10);
 }
