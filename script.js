1f32556f113bd6c5dcba1270116bde6fb236d170
Glitch (hello-webpage) <none>
2020-05-07 17:29:32 +0000

    🍵👱 Checkpoint ./script.js:15945879/355

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Name any p5.js functions we use in the global so Glitch can recognize them.
+/* global createCanvas, random, background, fill, color, rect, ellipse, stroke, noStroke, noFill, strokeWeight */
+
 function setup(){
   // Code here runs only once
-  createCanvas(400, 300)
+  createCanvas(300, 300)
 }
+
+let x = 50, y = 50;
+let w = 10, h = 50;
 
 function draw(){
   // Code here runs continuously
   background(50)
 
-  ellipse(50, 50, 50, 50)
+  ellipse(x++ % 200,
+          y++ % 200,
+          (10 + (w++ % 40)),
+          (50 - (h++ % 40)))
 
 }
