268bfcff2a28814da158138feee8a84eee783d5c
Glitch (hello-webpage) <none>
2020-05-08 19:37:20 +0000

    🌪🐄 Checkpoint ./script.js:15951174/5071

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 /* global
  *    HSB,
  *    background,
+ *    circle,
  *    color,
  *    colorMode,
  *    createCanvas,
@@ -18,11 +19,34 @@
  */
 
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(windowWidth, windowHeight);
 }
 
-let theme
+function windowResized() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+let isDarkTheme = false;
 
 function draw() {
+  isDarkTheme = mouseX > width / 2;
+
+  background(isDarkTheme ? 50 : 205);
   
+  stroke(isDarkTheme ? 205 : 50);
+  line(width / 2, 0, width / 2, height);
+  
+  fill(isDarkTheme ? 205 : 50);
+  text('Flip the switch', 10, 20);
+  
+  noStroke();
+  
+  fill(isDarkTheme ? 'blue' : 'red');
+  circle(width / 4, height / 2, width / 10);
+  
+  fill(isDarkTheme ? 'red' : 'blue');
+  circle(3 * width / 4, height / 2, width / 10);
+  
+  fill(isDarkTheme ? 205 : 50);
+  circle(mouseX, mouseY, width / 10);
 }
